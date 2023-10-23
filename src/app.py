9b9c62a128c474b199ea101cc3bd2f85300dde4b
superttogi <dohoikim@usc.edu>
2023-10-23 16:14:18 -0700

    Added the datatype parameter to app.py to distinguish the datasets

--- src/app.py
+++ src/app.py
@@ -1,23 +1,34 @@
 from kaping import KAPING
 from src.dataset import WebQSP
+from src.dataset import Mintaka
 import argparse
 import logging
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-def main(filename: str, outfile: str = "out.txt"):
+def main(filename: str, datatype: str, outfile: str = "out.txt"):
     logging.info(f"Read dataset : {filename}")
-    web_qsp = WebQSP(path=filename)
-    logging.info(f"Num questions: {len(web_qsp.data)}")
+    
+    if datatype.lower() == "WebQSP".lower():
+        inputdata = WebQSP(path=filename)
+        logging.info(f"data type = WebQSP")
+    elif datatype.lower() == "mintaka".lower():
+        inputdata = Mintaka(path=filename)
+        logging.info(f"data type = mintaka")
+    else:
+        logging.info(f"Wrong data type! Datatype should be 'WebQSP' or 'mintaka'.")
+        return
+        
+    logging.info(f"Num questions: {len(inputdata.data)}")
 
     kaping = KAPING()
 
     with open(outfile, 'w') as f:
-        datasets = web_qsp.data[:10]
+        datasets = inputdata.data[:10]
         for data in datasets:
             # TODO: insert delay between each call(RateLimitError: Rate limit reached for default-gpt-3.5-turbo in organization org-esrvXzMVq3Ig0JF7IPOfPHmb on requests per min. Limit: 3 / min.)
             prediction = kaping(data.question)  
-            logging.info(f"Prediction: {prediction}, Gold Answer: {[a.name for a in data.answers]}")
+            logging.info(f"Prediction: {prediction}, Gold Answer: {[a.name if hasattr(a, 'name') else a for a in data.answers]}")
             f.write(f"{data.qid}\t{data.question}\t{prediction}\n")
 
     # 3. Evaluate the results
@@ -25,11 +36,13 @@ def main(filename: str, outfile: str = "out.txt"):
     # TODO
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     parser = argparse.ArgumentParser(prog='Knowledge Augmented Language Model')
     parser.add_argument("filename")
+    parser.add_argument("datatype", choices=['WebQSP', 'mintaka'])
+    parser.add_argument("--head", type=int, default=None)
     parser.add_argument("--outfile", type=str, default="out.txt")
 
     args = parser.parse_args()
     logging.root.setLevel(logging.INFO)
-    main(args.filename, args.outfile)
+    main(args.filename, args.datatype, args.outfile)
