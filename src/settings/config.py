d38ec52966e387e5bf8f991f8a6ba42116601a5b
Dongwon Jung <dongwonj@usc.edu>
2023-10-25 16:15:33 -0700

    again

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,4 +1,4 @@
 class Config:
-    OPENAI_API_KEY = "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"        # DO NOT UPLOAD
+    OPENAI_API_KEY = ""        # DO NOT UPLOAD
     OPENAI_MODEL_NAME = "gpt-3.5-turbo"
     EMBEDDING_MODEL_NAME = "intfloat/e5-large-v2"
