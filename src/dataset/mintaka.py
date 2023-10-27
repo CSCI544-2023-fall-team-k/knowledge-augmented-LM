dda73f4bdde4ac37db068b2b0636309c293eeb8f
Dongwon Jung <dongwonj@usc.edu>
2023-10-26 20:30:37 -0700

    evaluation

--- src/dataset/mintaka.py
+++ src/dataset/mintaka.py
@@ -16,9 +16,27 @@ class Mintaka(KGQADataSet):
                 question_id = mintaka_data["id"]
                 raw_question = mintaka_data["question"]
                                 
-                answer_data = mintaka_data["answer"]["answer"]                
-                answers = [KGEntity(answer["label"]["en"]) if type(answer) is dict else str(answer) for answer in answer_data] if answer_data else []
-                
+                answer_data = mintaka_data["answer"]["answer"]
+                answers = []
+                if answer_data:
+                    for answer in answer_data:
+                        if type(answer) is dict:
+                            answers.append(KGEntity(answer["label"]["en"]))
+                        elif type(answer) is bool:
+                            # add additional answers yes/no for true/false question
+                            if answer == True:
+                                answers.append(KGEntity("True"))                         
+                                answers.append(KGEntity("Yes"))
+                            elif answer == False:
+                                answers.append(KGEntity("False"))   
+                                answers.append(KGEntity("No"))
+                            else:
+                                print("Invalid boolean value")
+                                continue
+                        else:
+                            answers.append(KGEntity(str(answer)))
+                else:
+                    continue
                 datasets.append(KGQAData(question_id, raw_question, answers))
 
         logging.info(f"number of parsed questions: {len(datasets)}")
