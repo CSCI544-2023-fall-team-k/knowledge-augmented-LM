9f6ca55b2b21435d81348122c6d20b22ab93d1d5
superttogi <dohoikim@usc.edu>
2023-11-15 17:42:41 -0800

    Add ComplexWebQuestions

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from kgppr import KGPPR
 from src.dataset import WebQSP
 from src.dataset import Mintaka
+from src.dataset import ComplexWebQ
 from kgppr.metrics import exact_matching
 from dspy.primitives import Example
 from kgppr.evaluate import Evaluate
@@ -17,6 +18,9 @@ def main(data: str, outfile: str = "evaluation_result.csv", num_test: int = 500)
     elif data.lower() == "mintaka".lower():
         dataset = Mintaka(path="resources/mintaka/data/mintaka_test.json")
         logging.info(f"data = mintaka")
+    elif data.lower() == "ComplexWebQ".lower():
+        dataset = ComplexWebQ(path="resources/ComplexWebQuestions/ComplexWebQuestions_train.json")
+        logging.info(f"data = ComplexWebQ")
     else:
         logging.info(f"Wrong data! It should be 'WebQSP' or 'mintaka'.")
         return
@@ -41,7 +45,7 @@ def main(data: str, outfile: str = "evaluation_result.csv", num_test: int = 500)
 
 if __name__ == '__main__':    
     parser = argparse.ArgumentParser(prog='Knowledge Augmented Language Model')
-    parser.add_argument("--data", choices=['WebQSP', 'mintaka'], default='WebQSP')
+    parser.add_argument("--data", choices=['WebQSP', 'mintaka', 'ComplexWebQ'], default='WebQSP')
     parser.add_argument("--outfile", type=str, default="evaluation_result.csv")
     parser.add_argument("--num_test", type=int, default=3)
 
--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .kgqa_data import KGQAData
 from .web_qsp import WebQSP
 from .mintaka import Mintaka
+from .complex_web_q import ComplexWebQ
